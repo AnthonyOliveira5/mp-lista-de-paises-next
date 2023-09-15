546e71678c4f82f02f406ff514d7425b02a0d42b
Anthony <anthony.oliveira5@hotmail.com>
2023-09-15 18:16:28 -0300

    Começando projeto que mostra a bandeira dos países em next.js

---
+++ app/page.tsx
@@ -0,0 +1,33 @@
+type Country = {
+  name: {
+    common: string;
+  };
+  translations: {
+    por: {
+      common: string;
+    }
+  }
+}
+
+async function getCountries(): Promise<Country[]>{
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  return response.json();
+}
+export default async function Home() {
+  const countries = await getCountries();
+
+  console.log(countries)
+  return (
+    <section className = "grid grid-cols-5 w-full container gap-2 mt-16">
+      {countries.map((country) => (
+        <article 
+          className="h-64 min-w-full p-2 bg-white border-2 rounded-xl" 
+          key={country.name.common}
+        >
+          
+          <h1>{country.translations.por.common}</h1>
+        </article>
+      ))}
+    </section>
+  );
+}
