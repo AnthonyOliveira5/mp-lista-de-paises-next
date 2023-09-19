6794b90961ffa0d419087090f925b8c04eaee606
Anthony <anthony.oliveira5@hotmail.com>
2023-09-19 16:27:04 -0300

    Trabalhando na pagina de detalhes

--- app/country/[name]/page.tsx
+++ app/country/[name]/page.tsx
@@ -1,3 +1,32 @@
-export default function country() {
-    return <h1>Country</h1>;
+import type {Country} from "@/app/page";
+import Link from "next/link";
+import Image from "next/image";
+
+async function getCountryByName(name:string): Promise<Country>{
+    const response = await fetch(`https://restcountries.com/v3.1/name/${name}?fullText=true`);
+    return (await response.json())[0];
+}
+
+export default async function CountryPage({
+    params:{name},
+}:{
+    params: {name : string};
+}) {
+    const country = await getCountryByName(name);
+
+    return (
+    <section className="flex flex-col container">
+
+        <h1 className="text-5xl text-center font-bold text-gray-800 my-16">{country.translations.por.common}</h1>
+        <Link className="flex items-center" href="/">
+            <Image src="/Vector.png" alt="Ícone de seta de voltar" width={24} height={24}></Image>
+            Voltar</Link>
+        <article className="flex justify-between min-w-full p-10 bg-white rounded-xl">
+
+        </article>
+
+    </section>
+    
+    
+    );
 }
