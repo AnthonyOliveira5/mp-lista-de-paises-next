03e19eb6c0405229d3c6c1868da23fef1159a920
Anthony <anthony.oliveira5@hotmail.com>
2023-09-20 23:37:27 -0300

    Adiciona informações na pagina detalhes e a bandeira

--- app/country/[name]/page.tsx
+++ app/country/[name]/page.tsx
@@ -1,32 +1,55 @@
-import type {Country} from "@/app/page";
+import type { Country } from "@/app/page";
 import Link from "next/link";
 import Image from "next/image";
 
-async function getCountryByName(name:string): Promise<Country>{
+async function getCountryByName(name: string): Promise<Country> {
     const response = await fetch(`https://restcountries.com/v3.1/name/${name}?fullText=true`);
     return (await response.json())[0];
 }
 
 export default async function CountryPage({
-    params:{name},
-}:{
-    params: {name : string};
+    params: { name },
+}: {
+    params: { name: string };
 }) {
     const country = await getCountryByName(name);
 
+    const formatador = Intl.NumberFormat("en", {notation: "compact"});
+
     return (
-    <section className="flex flex-col container">
+        <section className="flex flex-col container">
 
-        <h1 className="text-5xl text-center font-bold text-gray-800 my-16">{country.translations.por.common}</h1>
-        <Link className="flex items-center" href="/">
-            <Image src="/Vector.png" alt="Ícone de seta de voltar" width={24} height={24}></Image>
-            Voltar</Link>
-        <article className="flex justify-between min-w-full p-10 bg-white rounded-xl">
+            <h1 className="text-5xl text-center font-bold text-gray-800 my-16">{country.translations.por.common}</h1>
+            <Link className="flex items-center py-2" href="/">
+                <Image src="/Vector.png" alt="Ícone de seta de voltar" width={24} height={24}></Image>
+                Voltar</Link>
+            <article className="flex justify-between min-w-full p-10 bg-white rounded-xl">
+                <section>
+                    {country.capital &&(<h2 className="text-xl text-gray-800 mt-3">
+                        <b>🏙 Capital: </b> {country.capital}
+                    </h2>)}
+                    <h2 className="text-xl text-gray-800 mt-3">
+                        <b>🗺 Continente: </b> {country.region}{country.subregion &&(` - ${country.subregion}`)}
+                    </h2>
+                    <h2 className="text-xl text-gray-800 mt-3">
+                        <b>👩‍👩‍👧‍👦 População: </b>{formatador.format(country.population)}
+                    </h2>
+                    {country.languages && ( 
+                        <h2 className="text-xl text-gray-800 mt-3">
+                            <b>🗣 Línguas faladas: </b>
+                            <br/>
+                            {Object.values(country.languages).map((language) => (
+                                <span key={language} className="inline-block px-2 bg-indigo-700 mr-2 text-white text-sm rounded-full">
+                                    {language}
+                                </span>
+                                
+                            ))}
+                        </h2>
+                    )}
+                </section>
+                <div className="relative h-auto w-96 shadow-md"><Image src={country.flags.svg} alt={country.flags.alt} fill className="object-cover"/></div>
+            </article>
 
-        </article>
-
-    </section>
-    
-    
+        </section>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -13,36 +13,44 @@ export type Country = {
   flags: {
     svg: string;
     alt: string;
-  }
-}
+  };
 
-async function getCountries(): Promise<Country[]>{
+  capital: string;
+  region: string;
+  subregion: string;
+  population: number;
+  languages?: {
+    [key: string]: string;
+  };
+};
+
+async function getCountries(): Promise<Country[]> {
   const response = await fetch("https://restcountries.com/v3.1/all");
   return response.json();
-}
+};
 export default async function Home() {
   const countries = await getCountries();
 
   console.log(countries)
   return (
-    <section className = "grid grid-cols-5 w-full container gap-2 mt-16">
+    <section className="grid grid-cols-5 w-full container gap-2 mt-16">
       {countries.map((country) => (
-      <Link href={`/country/${country.name.common}`}>
-        <article 
-          className="h-64 min-w-full p-2 bg-white border-2 rounded-xl hover:border-indigo-200 transition-all hover:shadow-xl" 
-          key={country.name.common}
-        >
-          <div className="relative w-full h-40 p-2 overflow-hidden rounded-xl">
-          <Image 
-            src={country.flags.svg} 
-            alt={country.flags.alt} 
-            fill 
-            className="object-cover"/> 
-          </div>
-          
-          <h1 className="font-bold text-xl text-center mt-1">{country.translations.por.common}</h1>
-        </article>
-      </Link>        
+        <Link href={`/country/${country.name.common}`}>
+          <article
+            className="h-64 min-w-full p-2 bg-white border-2 rounded-xl hover:border-indigo-200 transition-all hover:shadow-xl"
+            key={country.name.common}
+          >
+            <div className="relative w-full h-40 p-2 overflow-hidden rounded-xl">
+              <Image
+                src={country.flags.svg}
+                alt={country.flags.alt}
+                fill
+                className="object-cover" />
+            </div>
+
+            <h1 className="font-bold text-xl text-center mt-1">{country.translations.por.common}</h1>
+          </article>
+        </Link>
       ))}
     </section>
   );
